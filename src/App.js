169559c2bf7c011ad663d843891ecd5c18303a72
Sophia Contesini <sophiamarques214@gmail.com>
2023-08-18 12:55:29 -0300

    feat: config index and app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Footer from './components/layout/Footer';
+import Navbar from './components/layout/Navbar';
+import Cadastrar from './pages/Cadastrar';
+import Clientes from './pages/Clientes';
+import Contato from './pages/Contato';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/cadastrar' element={<Cadastrar />} />
+          <Route path='/clientes' element={<Clientes />} />
+          <Route path='/contact' element={<Contato />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
